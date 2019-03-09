3a9ddf832160c4aa5762ca767b8079cad735f052
Nari421 <nari3451@gmail.com>
2019-03-09 16:28:55 +0900

    loginApi

--- src/lib/LoginApi.js
+++ src/lib/LoginApi.js
@@ -5,3 +5,6 @@ const url = "http://54.180.120.77:8080";
 export function postSingUpClient(userdata) {
     return axios.post(`${url}/realestate/client/signup/${userdata}`);
 }
+export function postSingUpAdmin(admindata) {
+    return axios.post(`${url}/realestate/admin/signup/${admindata}`);
+}
