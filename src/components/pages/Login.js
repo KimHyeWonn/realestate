3f50893f4abe1a0e567b311c689fcde4e961c400
Nari421 <nari3451@gmail.com>
2019-03-09 16:33:02 +0900

    정리

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import LoginPage from '../loginPage/LoginPage';
-
-class Login extends Component {
-  state={
-    id:'',
-  }
-  
-  usingIdPw = (data)=>{
-    console.log(data.id)
-    this.setState({
-      id:data.id
-    })
-    //this.props.setId(data.id)
-  }
-  render() {
-    return (
-      <div>
-        <LoginPage usingIdPw={this.usingIdPw}></LoginPage>
-      </div>
-    )
-  }
-}
-export default Login;
-=======
 import React, { Component } from 'react';
 import LoginPage from '../loginPage/LoginPage';
 import * as service from '../../lib/LoginApi';
@@ -59,4 +33,3 @@ class Login extends Component {
   }
 }
 export default Login;
->>>>>>> 560347a992719b63c0cc08357e99b422873be7b8
