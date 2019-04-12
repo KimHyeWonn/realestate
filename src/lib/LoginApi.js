300ac3a83a2a1a35df650fed0d58fdbd8191b387
Nari421 <nari3451@gmail.com>
2019-04-12 14:18:33 +0900

    login

--- src/lib/LoginApi.js
+++ src/lib/LoginApi.js
@@ -11,3 +11,8 @@ export function postSingUpAdmin(admindata) {
 export function getSignUp(userdata) {
     return axios.post(`${url}/realestate/sign/`,userdata[0]);
 }
+export function putCient(userdata){
+    return  axios.put(`${url}/realestate/sign/`,userdata,
+    {headers: {"Authorization": "abc:1553847812677:06f7e66c77b375ceefebc4393970b2b4"}
+})
+}
